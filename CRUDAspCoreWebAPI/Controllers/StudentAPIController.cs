de055ba1c4adbbf6631e401d11e18718db2df52a
rupjyoti921 <rupjyoti921@gmail.com>
2025-04-28 01:04:15 +0530

    completed CRUD operation using EF

---
+++ CRUDAspCoreWebAPI/Controllers/StudentAPIController.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using CRUDAspCoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAspCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+        private readonly MyDbContext context;
+
+        public StudentAPIController(MyDbContext context)
+        {
+            this.context = context;
+        }
+        //Get Request
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudets()
+        {
+            var data= await context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+        //Get Request with ID
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Student>>> GetStudetsByID(int id)
+        {
+            var data = await context.Students.FindAsync(id);
+            if (data == null)
+            {
+                return NotFound(); 
+            }
+            return Ok(data);
+        }
+
+        //Post Request
+        [HttpPost()]
+        public async Task<ActionResult<string>> CreateStudent(Student student)
+        {
+            await context.Students.AddAsync(student);
+            await context.SaveChangesAsync();
+            return Ok(student);
+            
+        }
+
+        //Put Request with id
+        [HttpPut("{id}")]
+        public async Task<ActionResult<string>> UpdateStudent(int id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(student).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(student);
+
+        }
+
+        //Delete Request with id
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<string>> DeleteStudent(int id)
+        {
+            var std = await context.Students.FindAsync(id);
+            if (std == null)
+            {
+                return BadRequest();
+            }
+            var temp = std;
+            context.Students.Remove(std);
+            await context.SaveChangesAsync();
+            return Ok(temp);
+        }
+    }
+} 
--- CRUDAspCoreWebAPI/Program.cs
+++ CRUDAspCoreWebAPI/Program.cs
@@ -1,8 +1,17 @@
+using CRUDAspCoreWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Add your DbContext service here
+var provider= builder.Services.BuildServiceProvider();
+var config = provider.GetRequiredService<IConfiguration>();
+builder.Services.AddDbContext<MyDbContext>(item =>
+    item.UseSqlServer(config.GetConnectionString("dbcs")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
