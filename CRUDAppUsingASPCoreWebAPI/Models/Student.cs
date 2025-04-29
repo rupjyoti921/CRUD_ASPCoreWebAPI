cf4c7b5f0b83db7bca7e700f595e158b0d9d2350
rupjyoti921 <rupjyoti921@gmail.com>
2025-04-29 21:13:10 +0530

    added student model class

---
+++ CRUDAppUsingASPCoreWebAPI/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUDAppUsingASPCoreWebAPI.Models
+{
+        public class Student
+        {
+            public int id { get; set; }
+            public string studentName { get; set; }
+            public string studentGender { get; set; }
+            public int age { get; set; }
+            public string standard { get; set; }
+            public string fatherName { get; set; }
+        }
+}
